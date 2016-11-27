a55f3462e4a6f6a3a7aeb50d169c2e25c6966486
kieranfraser <kfraser@tcd.ie>
2016-11-27 20:08:26 +0000

    Exp2 - PSO for membership functions

--- src/main/java/MastersProject/Nabs/App.java
+++ src/main/java/MastersProject/Nabs/App.java
@@ -211,6 +211,7 @@ public class App extends Application
 	  	    	//repo.saveRepoInstance();
 	  	    	//repo.activateNotificationListener();
 	  	    	
+	  	    	// Experiment 2!!
 	  	    	//experiment1();
 	  	    	experiment2();
 	  	    	
@@ -682,7 +683,8 @@ public class App extends Application
 	public static void experiment2(){
 		//Integer[] optimal = {3, 3, 1, 2, 2, 1, 2, 3, 2, 2, 1, 1, 3, 3, 3, 1, 1, 1, 3, 1, 4, 4, 5, 2, 5, 3, 3, 4, 5, 3, 2, 4, 2, 3, 5, 3, 3, 5, 3, 1, 4, 4, 5, 3, 2};
 		//Integer[] optimal = {1, 2, 3, 3, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1, 2, 4, 3, 3, 5, 2, 4, 3, 3, 3, 4, 3, 4, 4, 5, 2, 3, 4, 2, 4, 2, 5, 4, 3, 1, 3, 2};
-		Integer[] optimal = {2, 1, 1, 2, 2, 1, 2, 1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 2, 5, 1, 1, 2, 4, 1, 2, 4, 4, 3, 1, 1, 5, 1, 1, 4, 2, 4, 5, 2, 3, 5, 4, 3, 5, 1, 3};
+		
+		Integer[] optimal = {2, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 3, 3, 2, 1, 2, 2, 4, 1, 1, 4, 2, 4, 3, 2, 2, 4, 2, 2, 4, 3, 5, 5, 1, 2, 1, 4, 3, 4, 3, 2, 3, 3, 1};
 		relevantUsers = findRelevantUsers();
 		
 		ArrayList<User> givenUser = new ArrayList<User>();
